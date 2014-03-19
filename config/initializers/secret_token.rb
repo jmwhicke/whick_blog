9b2d828f3d98e93f755cbc2077ce4f66b6fc5cee
Jorden Whicker <jmwhicke@asu.edu>
2014-03-19 14:56:41 -0700

    Commit all files... hopefully.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhickBlog::Application.config.secret_key_base = 'e0e1099cfe16a8d43850b43830bef8c255a4f2ce956421366da8389cc804bff3e59ae63ea03b640536952e27f0b2e08b2be3dac35547254928575e9f70271061'
